85f2743a741e833d216c8a9d50c28d33dbcd15f7
Aleksey Krasavin <142975839+hRacc00n@users.noreply.github.com>
2024-09-19 16:14:36 +0300

    Добавляет интерактинвности

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,11 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+const burgerButton = document.querySelector('.toggle-burger');
+const headerButtonContainer = document.querySelector('.header__button-container');
+
+headerButtonContainer.addEventListener('click', () => {
+  headerButtonContainer.classList.toggle('header__button-container--open');
+  burgerButton.classList.toggle('toggle-burger--open');
+});
