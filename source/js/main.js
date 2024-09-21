6662a7889bfdd24de3c753715dcac4f84404f124
hRacc00n <MrAlexKr@gmail.com>
2024-09-21 19:57:37 +0300

    Добавляет интерактивности в меню

--- source/js/main.js
+++ source/js/main.js
@@ -5,8 +5,17 @@
 
 const burgerButton = document.querySelector('.toggle-burger');
 const headerButtonContainer = document.querySelector('.header__button-container');
+const headerMenu = document.querySelector('.header__menu');
 
 headerButtonContainer.addEventListener('click', () => {
   headerButtonContainer.classList.toggle('header__button-container--open');
   burgerButton.classList.toggle('toggle-burger--open');
+  headerMenu.classList.toggle('header__menu--open');
+});
+
+headerMenu.addEventListener('click', (evt) => {
+  if (evt.target.className === 'header__link header__link--submenu') {
+    evt.preventDefault();
+    evt.target.parentNode.classList.toggle('header__item--submenu-open');
+  }
 });
