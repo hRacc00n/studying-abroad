1b16eee9a7af3be1d10ff218e1b83c085a2d4abf
hRacc00n <MrAlexKr@gmail.com>
2024-09-24 20:01:43 +0300

    Верстает мобильную версию

--- source/js/main.js
+++ source/js/main.js
@@ -18,5 +18,35 @@ const swiperHero = new Swiper('.hero__swiper', {
   modules: [Pagination],
   pagination: {
     el: '.hero__shadow-pagination',
+    renderBullet: function(index) {
+      return `<span class="bullet-${index} swiper-pagination-bullet""></span>`;
+    },
+    clickable: true,
   },
+  loop: true,
+  breakpoints: {
+    320: {
+      simulateTouch: true,
+    },
+    1440: {
+      simulateTouch: false,
+    },
+  },
+});
+
+const heroWrapper = document.querySelector('.hero__wrapper');
+
+heroWrapper.addEventListener('click', (evt) => {
+  if (evt.target.className.includes('hero__pagination-button--1')) {
+    const bullet1 = document.querySelector('.bullet-0');
+    bullet1.click();
+  }
+  if (evt.target.className.includes('hero__pagination-button--2')) {
+    const bullet2 = document.querySelector('.bullet-1');
+    bullet2.click();
+  }
+  if (evt.target.className.includes('hero__pagination-button--3')) {
+    const bullet3 = document.querySelector('.bullet-2');
+    bullet3.click();
+  }
 });
