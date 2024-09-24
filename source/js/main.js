46a94405a48996078de759a98eaf9c4713d3b87d
Aleksey Krasavin <142975839+hRacc00n@users.noreply.github.com>
2024-09-24 15:39:16 +0300

    Добавляет карточки в блок hero

--- source/js/main.js
+++ source/js/main.js
@@ -2,8 +2,21 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
-
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { onButtonBurgerOpenMenu } from './on-button-burger-open-menu';
 import { headerButtonContainer } from './variables';
 
 headerButtonContainer.addEventListener('click', onButtonBurgerOpenMenu);
+
+// Слайдер
+
+const swiperHero = new Swiper('.hero__swiper', {
+  modules: [Pagination],
+  pagination: {
+    el: '.hero__shadow-pagination',
+  },
+});
