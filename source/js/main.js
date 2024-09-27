2f2db76f0910bf5b9f12f8bb5a986ceb946a70e2
Aleksey Krasavin <142975839+hRacc00n@users.noreply.github.com>
2024-09-27 16:35:44 +0300

    Добавляет селектор

--- source/js/main.js
+++ source/js/main.js
@@ -13,7 +13,10 @@ swiperHero.update();
 
 const aboutButton = document.querySelector('.about__button');
 const modal = document.querySelector('.modal');
+const form = modal.querySelector('.modal__form');
 const currentPhone = /^\d{10}$/;
+const phoneInput = modal.querySelector('.modal__input--phone');
+const modalSelect = modal.querySelector('.modal__select');
 
 aboutButton.addEventListener('click', () => {
   modal.classList.remove('modal--hide');
@@ -31,3 +34,35 @@ modal.addEventListener('click', (evt) => {
   }
 });
 
+const checkInput = (element, currentElement, evt) => {
+  if (!currentElement.test(element.value) || element.value === '') {
+    evt.preventDefault();
+    element.classList.add('modal__input--error');
+  }
+};
+
+phoneInput.addEventListener('input', () => {
+  if (currentPhone.test(phoneInput.value) || phoneInput.value === '') {
+    phoneInput.classList.remove('modal__input--error');
+  }
+});
+
+form.addEventListener('submit', (evt) => {
+  checkInput(phoneInput, currentPhone, evt);
+
+  if (modalSelect.value === 'empty') {
+    modalSelect.classList.add('modal__select--error');
+    evt.defaultPrevented();
+  }
+});
+
+const modalList = modal.querySelector('.modal__list');
+
+modalList.addEventListener('click', (evt) => {
+  if (evt.target.className.includes('modal__item')) {
+    modalSelect.value = evt.target.dataset.name;
+
+    modal.querySelector('.modal__item--active').classList.toggle('modal__item--active');
+    evt.target.classList.toggle('modal__item--active');тз
+  }
+});
